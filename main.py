5b1dc072f1a18e33c0f40daa2f435b9fccda2c26
Md Atik Hasan <atikhasan007@gmail.com>
2026-06-17 16:23:39 +0600

    tools created for agent

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.tools.tools import web_search , scrape_url
+
+
+# output = web_search.invoke("latest news on ai research?")
+# print(output)
+
+
+# result = scrape_url.invoke("https://www.artificialintelligence-news.com")
+
+# print(result)
